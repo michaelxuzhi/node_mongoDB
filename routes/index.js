058465e83cb38662f6b2b89a5e6b647923426bc2
michaelxuzhi <michaelxuzhi@foxmail.com>
2023-02-16 01:00:50 +0800

    revert: 路由index改动回退

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const { getOneAt, getAllAt } = require('../controller/at.controller');
-const { getOneRecord, getAllRecord } = require('../controller/record.controller');
+// const { getOneRecord, getAllRecord } = require('../controller/record.controller');
 /* GET home page. */
 // router.get('/', find);
-// router.get('/', getAllAt);
-router.get('/', getAllRecord);
-router.get('/', getOneAt);
+router.get('/', getAllAt);
+// router.get('/', getAllRecord);
+// router.get('/', getOneAt);
 
 module.exports = router;
